6344ae7af46bf58146e13cf8ddc1901dbc61d0bb
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-01 22:00:07 +0200

    Add files via upload

---
+++ add and remove in java/array.js
@@ -0,0 +1,21 @@
+// add and remove elements from array
+
+let names =['omda','ali','ahmed']
+
+// you want add name can you used 'push'
+names.push ('emad')
+console.log(names)
+
+// if you want add first eluemnts can you used 'unshift'
+
+let names2 =['ahmed','ali','omda']
+
+names2.unshift ('mohammed','abdo')
+console.log(names2)
+
+
+// remove array used 'shift'
+let names3 =['mena','toka','hager']
+names3.shift()
+console.log(names3)
+//If you want, you delete from the end used 'pop'
