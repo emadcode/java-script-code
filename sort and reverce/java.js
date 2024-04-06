bfccd16fc70d59ecb991f99a540f32d90633a544
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-06 03:20:12 +0200

    Add files via upload

---
+++ sort and reverce/java.js
@@ -0,0 +1,8 @@
+//If you want to reverse the order array used (reverse)
+//.........[   0    1        2       3        4]
+let names =['omda','ahmed','ali','mohammed','emad']
+
+console.log(names.reverse('mohammed',))
+// if you want sort array uesd (ssort)
+let arr=['4','8','6','5','9']
+console.log(arr.sort())
