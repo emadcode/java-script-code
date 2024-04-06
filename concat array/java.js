20b80400ec16c666aa031f07a6cac5c555823c80
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-06 03:39:01 +0200

    Add files via upload

---
+++ concat array/java.js
@@ -0,0 +1,8 @@
+// if you want concat array used (concat)
+//.........[   0    1        2       3        4]
+let arr2 =['omda','ahmed','ali','mohammed','emad']
+let arr1=['ali','mohammed','emad']
+
+arr1+arr2;
+
+console.log(arr2.concat(arr1))
