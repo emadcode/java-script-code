d474e1f62c11890f35f52011e0aa2bcba94f0bca
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-10 09:38:17 +0200

    Add files via upload

---
+++ switch in java script/script.js
@@ -0,0 +1,15 @@
+let role= prompt('what is your role?')
+switch (role)
+{
+    case 'admin':
+    document.write('create, update, admin  ');
+
+    break; // if you want add more user 
+    case 'editor':
+    document.write ('hello editor ');
+
+    
+    default:
+        document.write('hello user')
+
+}
