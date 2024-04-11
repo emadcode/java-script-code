86622521d0d2a1f89ee9a8a7ea63a9540a51fc05
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-11 07:33:50 +0200

    Add files via upload

---
+++ loop in java/script.js
@@ -0,0 +1,22 @@
+// start, conditions,steps
+//0
+//1
+//2
+
+for(let i =0;i<3; i+=1)
+{
+    console.log("hello Emad coder")
+}
+
+//0
+//1
+//2
+//(i)=
+//0
+//1
+//2
+for(let i = 0 ;i < 3 ; i+=1)
+{
+    console.log(i);
+}
+
