224486d1c80772e1b313da8be917c3a68122505f
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-12 11:13:07 +0200

    Add files via upload

---
+++ loops/script.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+//while loop: Executes a block of code as long as the specified condition evaluates to true.
+while (condition) {
+    // code to be executed
+}
+
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+//for...in loop: Used to iterate over the properties of an object.
+for (variable in object) {
+    // code to be executed
+}
