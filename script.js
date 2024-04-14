ad87066fd7bf342b58eab5e428bfab68376c3b89
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-14 07:46:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+//nastedloop in java script
+let cars =['bmw','mercedes','honda'];
+let models =[2022,2020,2024];
+let colors =['black','red','blue'];
+for (let o =0;o< cars.length;o++)
+{
+    console.log('car: ${cars[o]}');
+
+    for(let j=0;j <models.length;j++)
+{
+
+}
+    console.log('models:${models[j]}')
+
+}
+for(let x =0; x< colors.length; x++)
+{
+    console.log('colors: ${colors[x]}')
+    
+}
+{
+console.log('______________');
+}
