23e20f07263f137c34073104ffb60d9e7bfb09ff
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-15 06:51:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-
-//nastedloop in java script
-let cars =['bmw','mercedes','honda'];
-let models =[2022,2020,2024];
-let colors =['black','red','blue'];
-for (let o =0;o< cars.length;o++)
-{
-    console.log('car: ${cars[o]}');
-
-    for(let j=0;j <models.length;j++)
-{
-
-}
-    console.log('models:${models[j]}')
-
-}
-for(let x =0; x< colors.length; x++)
-{
-    console.log('colors: ${colors[x]}')
-    
-}
-{
-console.log('______________');
-}
+// while and do hhile in java script in loops 
+let i = 0;
+do{
+    document.write('<h1>hello omda how are you</h1>')
+    i++;
+}while(false);
+document.write(i);//here i bolean 1
+// and this code is print hello omda Just once 
+// do is print enything just once
+//wile The condition is if the verifier does not execute the code
