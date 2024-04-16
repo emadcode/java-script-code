9c593665fe5262519d42f816ae3202195450aeb9
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-16 06:13:54 +0200

    Add files via upload

---
+++ function in java script/script.js
@@ -0,0 +1,11 @@
+function pro( price, taxes,ads){
+    let pro = price * taxes;
+    let result =pro + ads;
+    console.log(result);
+
+}
+pro(200,10,300)
+pro(200,10,38)
+pro(200,10,30)
+
+//function Write a code once 
