a95bc22087e3c10de5c2cc05b85942fb3459c4e1
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-17 07:05:26 +0200

    Add files via upload

---
+++ hoisting in java script/script.js
@@ -0,0 +1,19 @@
+console.log  (         x( )        );
+function x()
+{
+    function y (){return 1}
+    return y();
+    function y(){return 0}
+}
+
+//A question from Microsoft abour hosting in java script
+//javascript is see code 
+
+function x()
+{
+    function y (){return 1}
+    function y(){return 0} 
+       return y();
+
+}
+console.log  (         x( )        );
