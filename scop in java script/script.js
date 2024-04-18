ae959836797c9c615f6f99510d2bd9e629b17235
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-18 06:23:19 +0200

    Add files via upload

---
+++ scop in java script/script.js
@@ -0,0 +1,12 @@
+(function ()
+{
+    console.log('hello omda');
+    console.log('hello emad');
+
+    var x =1;
+    let s =0;
+    console.log('hello');
+    console.log ('hello');
+
+})();
+// functions in scop
