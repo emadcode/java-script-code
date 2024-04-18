d6d96856473fcf1784295a6a91f57f95acb31145
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-18 08:33:46 +0200

    Add files via upload

---
+++ arguments in java script/script.js
@@ -0,0 +1,15 @@
+
+
+
+function calc(    ...numbers    )
+{
+    let result =0;
+    for(let i = 0; i <numbers.length; i++){
+        result += numbers[i];
+
+
+    }
+    console.log(result);
+}
+calc(10,20);
+//How I did a function by receiving an infinite number
