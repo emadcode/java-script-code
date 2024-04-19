cdf119666a7a3ddbb90869d2bbcb03e851438737
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-19 08:29:13 +0200

    Add files via upload

---
+++ object in javascript/script.js
@@ -0,0 +1,13 @@
+let car = { //here if you want create object        
+   title:'hinda',
+     color: 'red',
+     price: '4000000',
+    model:['2020','2023'],
+    hello:function(){
+
+        return 'hello';
+    }
+
+};
+
+console.log(car.hello())
