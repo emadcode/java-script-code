7f53d2ee716783a0f5b2e546fa422cba5d9ab021
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-20 12:14:14 +0200

    Add files via upload

---
+++ object in 3/script.js
@@ -0,0 +1,16 @@
+let o1 ={
+    num1:1,
+    hello:function(){
+        return 'hello ';
+    }
+}
+let o2 ={
+    num2:2
+
+}
+let a4 = Object.assign(o1,o2,{
+
+    
+});
+ console.log(a4);
+ // Consolidation in object used (assign)
