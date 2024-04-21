a595e4403996937d27b5fc1d37fb691eaff83f86
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-21 23:45:52 +0200

    Add files via upload

---
+++ Dom in javascript/script.js
@@ -0,0 +1,35 @@
+//for 
+for (let i = 0; i< 5; i++)
+{
+  console.log (i);
+
+}
+// print 0 1 2 3 4
+
+// do while 
+let omda =0;
+do {
+
+omda++;
+console.log(omda);
+} while (omda <5);
+
+//wile
+let user =0;
+while (user < 6){
+  user++;
+  console.log(user);
+}
+
+//for in
+const arr =[3,4,5,];
+arr.foo ='hello omda how are you ';
+for (let i of arr ){
+  console.log (i);
+}
+// for of
+const arr =[3,4,3];
+arr.foo ='hello';
+for (let i of arr){
+  console.log(i);
+}
