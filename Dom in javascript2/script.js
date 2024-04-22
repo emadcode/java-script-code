1719e92a42b51c8edbb3f266e8f7d5b1cc940673
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-22 12:31:57 +0200

    Add files via upload

---
+++ Dom in javascript2/script.js
@@ -0,0 +1,21 @@
+ 
+//1- الوصول الى العنصر من خلال ال id  الخاص به
+document.getElementById()
+//2- الوصول الى العنصر من خلال ال class name  الخاص به
+document.getElementsByClassName()
+//3- الوصول الى العنصر من خلال ال tag name  الخاص به
+document.getElementsByTagName()
+//4- الوصول الى العنصر من خلال ال selector
+document.querySelector()
+//5- الوصول الى العنصر من خلال ال collection
+document.querySelectorAll()
+//6- الوصول الى الـ body
+document.body
+//7- الوصول الى الـ title
+document.title
+//8- الوصول الى الـ images
+document.images
+//9- الوصول الى الـ links
+document.links
+//10- الوصول الى الـ forms
+document.forms
