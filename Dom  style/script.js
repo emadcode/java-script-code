49503ae0f1328b74fc4f0e31c910307d0de6e093
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-23 15:40:18 +0200

    Add files via upload

---
+++ Dom  style/script.js
@@ -0,0 +1,11 @@
+ let containar =document.getElementById('containar');
+
+ containar.innerText ='hello world';
+
+ // element.styel
+
+ containar.style.backgroundColor='#444';
+ containar.style.color='#fa0';
+  containar.style.padding='10px';
+ containar.style.borderLeft='4px solid #fa0';
+
