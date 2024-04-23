e56bafb624bac0534fa6b4b60ae22c326dbbd1e4
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-23 09:36:35 +0200

    Add files via upload

---
+++ Dom in javascript3/script.js
@@ -0,0 +1,7 @@
+let omda =document.getElementById('omda')
+
+console.log(omda.previousElementSibling)
+console.log(omda.nextElementSibling)
+omda.nextElementSibling.innerHTML="test"
+console.log(omda.parentElement)//fhater element
+//sibling and parent
