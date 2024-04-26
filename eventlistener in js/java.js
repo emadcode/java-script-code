7bb6c389850974e5344fd75a09d1918f8b964a70
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-26 17:46:51 +0300

    Add files via upload

---
+++ eventlistener in js/java.js
@@ -0,0 +1,22 @@
+let button =document.getElementById('button');
+
+//button.addEventListener('click',function(){
+  //  console.log ('omda');
+//})
+
+
+//
+
+button.addEventListener('click', function(){
+    document.body.style.background='blue'
+
+})
+button.addEventListener('click', function(){
+    button.style.textEmphasisPosition='red'
+
+    
+})
+button.addEventListener('click', function(){
+    button.style.color='red';
+    
+})
