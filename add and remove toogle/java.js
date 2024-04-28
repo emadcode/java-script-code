83b4aca8e8edac736d51e7df5f954d90fceaf864
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-04-29 00:01:05 +0300

    Add files via upload

---
+++ add and remove toogle/java.js
@@ -0,0 +1,10 @@
+let omda=document.getElementById('omda');
+
+
+omda.onclick = function(){
+    omda.classList.add('name')
+
+}
+omda.oncontextmenu = function(){
+    omda.classList.remove("name")
+}
