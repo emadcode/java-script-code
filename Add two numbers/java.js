e8e0c6a0707af77af4b0fea78e0a0549da51d6a5
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-05-10 20:11:31 +0300

    Add files via upload

---
+++ Add two numbers/java.js
@@ -0,0 +1,15 @@
+
+//Add two numbers
+const number1 = 5;
+const number2 = 10;
+const  num1 =20;
+const num2 =30;
+
+// Add the numbers
+const sum = number1 + number2;
+const sum2 = num1-num2;
+//const sum = number1 + number2;
+//const sum = number1 / number2;
+//const sum2 = num1*num2;
+// Print the result
+console.log("The sum of the numbers is:", sum,sum2);
