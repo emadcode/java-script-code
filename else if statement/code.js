a2c16d816b3fcc98c93d628af67c2e1f6055e8c1
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-05-28 23:28:03 +0300

    Add files via upload

---
+++ else if statement/code.js
@@ -0,0 +1,12 @@
+//else if statements
+
+var score = 85;
+if (score >= 95) {
+    console.log("Congratulations on your excellent performance!");
+} else if (score >= 80) {
+    console.log("Good job, keep it up");
+} else if (score >= 70) {
+    console.log("You have a chance to improve.");
+} else {
+    console.log("You need to work harder.");
+}
