df81219045ddc67e80f92174ae6e0042b3ae6668
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-06-10 17:46:07 +0300

    Add files via upload

---
+++ proplem soliveng in js/script.js
@@ -0,0 +1,18 @@
+function binarySearch(arr, target) {
+    let start = 0;
+    let end = arr.length - 1;
+    
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    
+    return -1; // Return -1 if the element is not found
+}
