10f9086107c21aa81246dd42e50da8870a1d1d32
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-08-12 01:43:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-// Define the function to calculate the perimeter of a rectangle
-function calculateRectanglePerimeter(length, width) {
-    // Calculate the perimeter using the formula
-    let perimeter = 2 * (length + width);
-    
-    // Return the calculated perimeter
-    return perimeter;
+//var name="Emad";
+//rconsole.log(name);
+
+
+
+//let age =22;
+//console.log(age);
+
+
+function sayhelloomda(){
+    let greeting ="hello omda";
+    if (true){
+        var name ="Emad";
+        let age ="22";
+        console.log(greeting+","+ name);
+
+    }
+    console.log(name);
+
 }
-
-// Example usage of the function
-let length = 5;  // Example length
-let width = 3;   // Example width
-let perimeter = calculateRectanglePerimeter(length, width);
-
-console.log("The perimeter of the rectangle is: " + perimeter);
-//    The function calculateRectanglePerimeter takes two parameters: length and width.
-//Inside the function, it calculates the perimeter using the formula 2×(length+width)2×(length+width).
+sayhelloomda();
