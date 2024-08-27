a7ce6ae93c8e90051b8061e4e2a5afc4e7465594
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-08-27 22:43:56 +0300

    typeof

---
+++ omda.js
@@ -0,0 +1,9 @@
+console.log(typeof 55);
+console.log(typeof 42); // "number"
+console.log(typeof "Hello"); // "string"
+console.log(typeof true); // "boolean"
+console.log(typeof undefined); // "undefined"
+console.log(typeof null); // "object" (this is a known quirk in JavaScript)
+console.log(typeof {}); // "object"
+console.log(typeof []); // "object" (arrays are objects in JavaScript)
+console.log(typeof function(){}); // "function"
