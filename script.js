bde6dea102a77beda82d348f52f70681d4afbbd9
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-10-07 04:15:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-/*
-data types intro
--- string
--- number
--- array => object
---object
-*/
-console.log("omda holm");
-console.log(typeof "Emad Omda");
-console.log(typeof 5000);
-console.log(typeof 500.90);
-console.log(typeof[10,20,30]);
-console.log(typeof{nmae:"emad",age:22})
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    // Simulate a network request using setTimeout
+    setTimeout(() => {
+      let success = true;
+      
+      if (success) {
+        resolve("Data fetched successfully omda!");
+      } else {
+        reject("Failed to fetch data!");
+      }
+    }, 2000);
+  });
+}
+
+fetchData()
+  .then((data) => {
+    console.log(data); // "Data fetched successfully!"
+  })
+  .catch((error) => {
+    console.log(error); // "Failed to fetch data!" if an error occurs
+  });
